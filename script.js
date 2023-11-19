1cd9df987c0876ba82253b53f417483bacfa34c8
Trevor Danner <tjdanner01@gmail.com>
2023-11-19 14:52:34 -0600

    Changed up site, functional now.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+const contactButton = document.getElementById('contact-button');
+const endCTA = document.getElementById('end-cta');
+
 const welcomeImage = document.getElementsByClassName('welcome-image');
 const welcomeImageArray = Array.from(welcomeImage);
 const serviceCard = document.getElementsByClassName('service-card');
 const serviceCardArray = Array.from(serviceCard);
 
+contactButton.addEventListener('click', () => {
+  console.log('it works');
+  endCTA.scrollIntoView({ behavior: 'smooth' });
+});
+
 welcomeImageArray.forEach((element) => {
   element.addEventListener('mouseover', () => {
-    element.style.scale = '1.1';
+    element.style.scale = '1.3';
     element.style.transition = '400ms';
   });
   element.addEventListener('mouseout', () => {
